f6a112eac30e4c0c53059eb1e52b588a99744fdb
chengos74 <chengkoeng@gmail.com>
2022-07-28 13:51:24 +0200

    fichiers finaux

---
+++ models/cities.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose')
+
+var citySchema = mongoose.Schema({
+    name: String,
+    desc: String,
+    img: String,
+    temp_min: Number,
+    temp_max: Number,
+})
+
+var cityModel = mongoose.model('cities', citySchema)
+
+module.exports = cityModel;
